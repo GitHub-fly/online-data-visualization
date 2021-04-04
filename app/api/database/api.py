7ccd4bc83e2d9a0357a131aa13e5fa7d9ff7efc1
flygit <1224178565@qq.com>
2021-04-04 21:54:27 +0800

    新增MySQL数据库连接交互

--- app/api/database/api.py
+++ app/api/database/api.py
@@ -1,6 +1,7 @@
 from . import database
 from flask import Flask, request
 from sqlalchemy import create_engine
+import pymysql
 
 from ...utils.APIResponse import APIResponse
 
@@ -10,17 +11,22 @@ conn = None
 
 @database.route("/conn", methods=["POST"])
 def change_sql_conn():
-    # get_json() 返回 dict 类型
     global conn
-    conn_obj = request.get_json()
-
-    engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(
-        conn_obj['userName'], conn_obj['password'], conn_obj['host'], conn_obj['port'], conn_obj['database']
-    ))
+    conn_obj = request.get_json()  # get_json() 返回 dict 类型
+    sql_type = conn_obj['sqlType'].lower()
     try:
-        conn = engine.connect()
+        if sql_type == 'mysql':
+            engine = create_engine('{}+pymysql://{}:{}@{}:{}/{}'.format(
+                str(conn_obj['sqlType']).lower(), conn_obj['userName'], conn_obj['password'], conn_obj['host'],
+                conn_obj['port'], conn_obj['database']
+            ))
+            conn = engine.connect()
+        elif sql_type == 'postgresql':
+            engine = create_engine('{}://{}:{}@{}:{}/{}'.format(
+                str(conn_obj['sqlType']).lower(), conn_obj['userName'], conn_obj['password'], conn_obj['host'],
+                conn_obj['port'], conn_obj['database']
+            ))
+            conn = engine.connect()
     except BaseException:
-        res = APIResponse(400, '连接失败')
-        return res.body()
-    res = APIResponse(200, '连接成功')
-    return res.body()
+        return APIResponse(400, '连接失败').body()
+    return APIResponse(200, '连接成功').body()
